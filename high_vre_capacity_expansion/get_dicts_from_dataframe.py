512eaf6ee9350175a5dde4ef96e91dd3fc5c98d8
Srihari Sundar <sriharisundar95@gmail.com>
2021-08-18 16:25:49 -0400

    Modify get dict from data frame to use numpy and get massive speed up.

--- high_vre_capacity_expansion/get_dicts_from_dataframe.py
+++ high_vre_capacity_expansion/get_dicts_from_dataframe.py
@@ -1,9 +1,10 @@
 def get_dicts_from_dataframe(dframe):
     names = dframe.columns.to_list()
 
-    param_set = {}
-    for i in range(1, dframe.columns.size+1):
-        for t in range(1, dframe.index.size+1):
-            param_set[(i, t)] = dframe.iloc[t-1][dframe.columns[i-1]]
+    dframe_np = dframe.to_numpy().T
+    param_set = {(i + 1, t + 1): dframe_np[i, t] \
+                 for t in range(dframe_np.shape[1]) \
+                 for i in range(dframe_np.shape[0]) \
+                 }
 
     return names, param_set
