ef0c69df8ff0d226b8e9d042f53cad73db3131b8
Srihari Sundar <sriharisundar95@gmail.com>
2022-03-09 15:21:02 -0500

    Ammend get dict to have 2d array.

--- high_vre_capacity_expansion/get_dicts_from_numpy.py
+++ high_vre_capacity_expansion/get_dicts_from_numpy.py
@@ -1,10 +1,14 @@
 def get_dicts_from_numpy(nparray):
 
-    if nparray.ndim is 1:
+    if nparray.ndim == 1:
         array_dict = {(i+1):val for i,val in enumerate(nparray)}
-    elif nparray.ndim is 2:
-        array_dict = {(i+1,j+1):val \
-                      for j,val in enumerate(row)\
-                      for i,row in enumerate(nparray)}
+
+    elif nparray.ndim == 2:
+        array_dict = {(i+1,j+1):nparray[i,j] \
+                      for j in range(nparray.shape[1])\
+                      for i in range(nparray.shape[0])}
+
+    else:
+        return
 
     return array_dict
